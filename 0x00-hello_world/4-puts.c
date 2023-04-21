1278d8ab00804f9acbcc38dbc81343f573ee8706
amandamaziba <amandamaziba@gmail.com>
2023-04-21 07:32:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the fuction will print a massage using puts function
+ *
+ * Return: 0(Successs)
+ */
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
