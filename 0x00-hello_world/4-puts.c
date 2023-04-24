e880ac05e4193562a692bb5abe6655a7772b6218
amandamaziba <amandamaziba@gmail.com>
2023-04-24 14:19:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: 0(Successs)
  */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle \n");
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
