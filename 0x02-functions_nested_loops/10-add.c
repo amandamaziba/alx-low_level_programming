d5555be3e847095c755b33a1b205309705730ebb
amandamaziba <amandamaziba@gmail.com>
2023-04-26 18:00:30 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*add - add two intergers
+*@i: interger to be added
+*@x: interger to be added
+*Return: interger results
+*/
+int add(int i, int x)
+{
+	return (i + x);
+}
