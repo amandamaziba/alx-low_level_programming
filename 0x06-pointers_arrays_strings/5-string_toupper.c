57a009f271a6949ea4dd61a924fe6a9f0f3b285b
amandamaziba <amandamaziba@gmail.com>
2023-05-04 11:44:42 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - compares two strings
+ * @a: pointer to string
+ * Return: uppercase string
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
