97d0091c6a6e573e3fa168a7eb895b89196f2df6
amandamaziba <amandamaziba@gmail.com>
2023-05-04 12:50:21 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,16 +12,21 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[0] >= 97 && str[0] <= 122)
+		while (!(str[i] >= 97 && str[0] <= 122))
+			i++;
+		if (str[i] == ',' || 
+		str[i] == ';' || 
+		str[i] == '.' || 
+		str[i] == '!' || 
+		str[i] == '?' || 
+		str[i] == '"' || 
+		str[i] == '(' || 
+		str[i] == ')' || 
+		str[i] == '{' || 
+		str[i] == '}' || 
+		i == 0)
 		{
-			str[0] = str[0] - 32;
-		}
-		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
+			str[i + 1] = str[i + 1] - 32;
 		}
 		i++;
 	}
