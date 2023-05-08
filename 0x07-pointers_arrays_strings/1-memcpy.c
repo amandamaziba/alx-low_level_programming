ef3cf91e562be36503715cedcea0323e9eef11e0
amandamaziba <amandamaziba@gmail.com>
2023-05-08 11:55:24 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset- copies memory area
+ * @dest: memory area to
+ * @src: memory area from
+ * @n: bytes
+ * Return: pointer to the memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
