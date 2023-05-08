ac42af0eee7d799b9bd55aac6d49e86766406978
amandamaziba <amandamaziba@gmail.com>
2023-05-08 12:38:09 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - returns the length of a string
+ * @s: input
+ * @accept: input
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
