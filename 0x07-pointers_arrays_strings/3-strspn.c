e91b9c9cf9bba1758999fe25be916a93a8ce9239
amandamaziba <amandamaziba@gmail.com>
2023-05-08 12:15:58 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int a = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] >= '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				a++;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (a);
+			}
+		}
+		s++;
+	}
+	return (a);
+}
