41a42ad38bc033439c4d5d790c4f94a2afab6f38
amandamaziba <amandamaziba@gmail.com>
2023-05-08 13:04:01 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - returns the length of a string
+ * @a: an array
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[i][x]);
+		}
+		_putchar('\n');
+	}
+}
